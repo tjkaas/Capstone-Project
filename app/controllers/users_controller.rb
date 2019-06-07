7f19a9614c9e921b35c5901e2631bb478943db7c
Tristan Kaas <tjkaas96@aol.com>
2019-06-07 15:44:49 +0000

    login page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,4 +8,7 @@ class UsersController < ApplicationController
     
     def edit
     end
+    
+    def login
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  get '/users', to: 'users#index' 
-  get '/users/new', to: 'users#new'
-  get '/users/edit', to: 'users#edit'
+  get '/users',       to: 'users#index' 
+  get '/users/new',   to: 'users#new'
+  get '/users/edit',  to: 'users#edit'
+  get '/users/login', to: 'users#login'
 end
