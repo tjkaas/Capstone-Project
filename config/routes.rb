f3fc0569e5c2679340e5ddc2d7290bbabf961807
EC2 Default User <ec2-user@ip-172-31-28-187.us-east-2.compute.internal>
2019-06-07 16:59:15 +0000

    added projet model

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
+  resources :todo_lists
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root "todo_lists#index"
   get '/users',       to: 'users#index' 
   get '/users/new',   to: 'users#new'
   get '/users/edit',  to: 'users#edit'
---
+++ db/migrate/20190607160433_create_projects.rb
@@ -0,0 +1,12 @@
+class CreateProjects < ActiveRecord::Migration[5.1]
+  def change
+    create_table :projects do |t|
+      t.string :task
+      t.string :title
+      t.text :notes
+      t.date :completed
+
+      t.timestamps
+    end
+  end
+end
