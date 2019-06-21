f7a9b9e5b8e4263edeaccc6d393aa4b9ae6dba89
EC2 Default User <ec2-user@ip-172-31-28-187.us-east-2.compute.internal>
2019-06-21 16:20:53 +0000

    more project stuff

--- app/controllers/project_controller.rb
+++ app/controllers/project_controller.rb
@@ -1,2 +1,32 @@
 class ProjectController < ApplicationController
+  before_action :logged_in_user, only: [:create, :destroy]
+  before_action :correct_user,   only: :destroy
+
+  def create
+    @project = current_user.projects.build(project_params)
+    if @project.save
+      flash[:success] = "Project created!"
+      redirect_to root_url
+    else
+      @feed_items = []
+      render 'static_pages/home'
+    end
+  end
+
+  def destroy
+    @project.destroy
+    flash[:success] = "Project deleted"
+    redirect_to request.referrer || root_url
+  end
+
+  private
+
+    def project_params
+      params.require(:project).permit(:content)
+    end
+    
+    def correct_user
+      @project = current_user.project.find_by(id: params[:id])
+      redirect_to root_url if @project.nil?
+    end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,5 @@
 class UsersController < ApplicationController
+  before_action :logged_in_user, only: [:index, :edit, :update, :destroy]
     
     def index
     end
@@ -6,6 +7,11 @@ class UsersController < ApplicationController
     def new
       @user = User.new
     end
+    
+    def show
+    @user = User.find(params[:id])
+    @projects = @user.projects.paginate(page: params[:page])
+  end
 
     
     def edit
